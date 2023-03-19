79daa728802bd4dc28425387e30cb705d255b1ff
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-19 10:48:09 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,24 @@ public class Main {
         // Skapa en klass som kan användas för att representera ett rationellt tal
         // Klassen skall spara två heltal täljaren och nämnaren
 
+        RatNum num1 = new RatNum();
+        num1.p = 5;
+        num1.q = 9;
 
+        RatNum num2 = new RatNum();
+        num2.p = 1;
+        num2.q = 3;
+
+        RatNum num3 = new RatNum();
+        num3.p = 7;
+        num3.q = 12;
+
+        System.out.format("%d/%d", num1.p, num1.q);
     }
+}
+
+class RatNum {
+    // p / q
+    int p;
+    int q;
 }
