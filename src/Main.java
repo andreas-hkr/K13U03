99fb39504966afc3f8a7cb1897ea188c40ff3760
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-19 10:41:29 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skapa en klass som kan användas för att representera ett rationellt tal
+        // Klassen skall spara två heltal täljaren och nämnaren
+
+
+    }
+}
